83774d7d3c0ab689a67681cc6e12ca4ee86169e6
DuncanMcCabe96 <duncan96mccabe@gmail.com>
2025-02-06 18:09:40 -0700

    First Javascript practice

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var A = "I like movies";
+window.alert(A);
+var B = "I\'m cool" + " as a cucumber"
+document.write(B)
+var Sport = "Hockey", TeamA = " Avalanche ", TeamB = "Hurricanes", TeamC = "Lightning";
+document.write(TeamA);
+document.write(3 + 3);
+
+function My_First_Function(){ 
+    var str = "This is the button text"; //What the button will display when clciked
+    document.getElementById("Button_Text").innerHTML = str;
+}
