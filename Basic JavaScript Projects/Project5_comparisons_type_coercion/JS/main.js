63e18121562ab14a919c45ce832ce7278624e7d8
DuncanMcCabe96 <duncan96mccabe@gmail.com>
2025-02-15 18:42:43 -0700

    added a handulf of boolen logic code

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,33 @@
+document.writeln(typeof "kuhikuhkuh")
+
+document.writeln(6 + "10")
+
+function practice_Function(){
+    document.getElementById("practice").innerHTML = isNaN('This is words');
+}
+
+document.writeln(-5E430);
+document.writeln(5E430);
+document.writeln(2 > 1);
+document.writeln(2 < 1);
+console.log(2 +2);
+console.log(2 > 5);
+document.writeln(2 == 1);
+document.writeln(2 == 2);
+x=10;
+y=10
+document.writeln(x===y);
+a="word";
+b="word";
+c="letter"
+document.writeln(b===a);
+document.writeln(b===x);
+document.writeln(b===c);
+document.writeln(5 > 2 && 10 > 5);
+document.writeln(5 > 2 && 10 < 5);
+document.writeln(5 > 2 || 10 < 5);
+document.writeln(5 < 2 || 10 < 5);
+
+function not_Function(){
+    document.getElementById("Not").innerHTML=!(20>10)
+}
